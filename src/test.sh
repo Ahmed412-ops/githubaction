a5c315670e843f646671b1f45a63bc3dc46d7cf0
Ahmed412-ops <ahmed.mostafa.elsayed45@gmail.com>
2025-07-08 10:14:57 +0300

    add symbol

--- src/test.sh
+++ src/test.sh
@@ -4,8 +4,8 @@ expected_output="Hello, Ahmed!"
 output=$(python ./src/main.py | tr -d '\n')
 
 if [ "$output" = "$expected_output" ]; then
-    echo "Test passed!"
+    echo "✔ Test passed!"
 else
-    echo "Test failed! Expected '$expected_output' but got '$output'."
+    echo "❌ Test failed! Expected '$expected_output' but got '$output'."
     exit 1
 fi
